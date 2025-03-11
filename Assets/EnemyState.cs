2e3f7ecda169c1b638c85a5900ccf13e73cb7a36
Bao Thuong <baothuong18022003@gmail.com>
2025-03-11 23:26:07 +0700

    done enemy

--- Assets/EnemyState.cs
+++ Assets/EnemyState.cs
@@ -6,24 +6,27 @@ using UnityEngine;
 public class EnemyState 
 {
     protected EnemyStateMachine stateMachine;
-    protected Enemy enemy;
+    protected Enemy enemyBase;
+    protected Rigidbody2D rb;
 
     protected bool triggerCalled;
     private string animBoolName;
 
     protected float stateTimer;
 
-    public EnemyState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBollName)
+    public EnemyState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBollName)
     {
-        this.enemy = _enemy;
+        this.enemyBase = _enemyBase;
         this.stateMachine = _stateMachine;
         this.animBoolName = _animBollName;
     }
 
     public virtual void Enter()
     {
-        triggerCalled = false;
 
+        triggerCalled = false;
+        rb = enemyBase.rb; 
+        enemyBase.anim.SetBool(animBoolName, true);
     }
 
     public virtual void Update()
@@ -32,6 +35,11 @@ public class EnemyState
     }
     public virtual void Exit()
     {
-        enemy.anim.SetBool(animBoolName, false);
+        enemyBase.anim.SetBool(animBoolName, false);
+    }
+
+    public virtual void AnimationFinishTrigger()
+    {
+        triggerCalled = true;
     }
 }
